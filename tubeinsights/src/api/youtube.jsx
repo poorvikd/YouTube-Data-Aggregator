9bc7135afa052c4f5ee4b71f05c0b0f3662a034c
poorvikd <64829799+poorvikd@users.noreply.github.com>
2024-02-26 21:26:13 -0700

    Generated env file

--- tubeinsights/src/api/youtube.jsx
+++ tubeinsights/src/api/youtube.jsx
@@ -1,8 +1,8 @@
-// Desc: This file contains the YouTube API configuration. It uses the axios library to make requests to the YouTube API. It exports the axios instance with the base URL and the API key.
+// Desc: This file contains the YouTube API configuration. It uses the axios library to make requests to the YouTube API. It exports the axios instance with the base URL and the API key read from the env file.
 import axios from 'axios';
 
 
-const KEY = 'AIzaSyBdh1Kv798gtMp3feAN2eoauJG0pod-9Vg'
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
